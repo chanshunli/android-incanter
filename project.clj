f7abe02217e6da73c1a063ed38ad4b5050644b34
Steve Chan <chanshunli@gmail.com>
2018-04-18 18:27:28 +0800

    Update incanter: push to clojars, 1.9.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject incanter "1.9.3-SNAPSHOT"
+(defproject steve-incanter "1.9.3-SNAPSHOT"
   :description "Incanter is a Clojure-based, R-like statistical programming and data visualization environment."
   :url "http://incanter.org/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/incanter/incanter"}
   :min-lein-version "2.0.0"
-  :dependencies [[incanter/incanter-core "1.9.3-SNAPSHOT"]
-                 [incanter/incanter-io "1.9.3-SNAPSHOT"]
-                 [incanter/incanter-charts "1.9.3-SNAPSHOT"]
-                 [incanter/incanter-mongodb "1.9.3-SNAPSHOT"]
-                 [incanter/incanter-pdf "1.9.3-SNAPSHOT"]
-                 [incanter/incanter-svg "1.9.3-SNAPSHOT"]
-                 [incanter/incanter-latex "1.9.3-SNAPSHOT"]
-                 [incanter/incanter-excel "1.9.3-SNAPSHOT"]
-                 [incanter/incanter-sql "1.9.3-SNAPSHOT"]
-                 [incanter/incanter-zoo "1.9.3-SNAPSHOT"]
+  :dependencies [[steve-incanter/incanter-core "1.9.3-SNAPSHOT"]
+                 [steve-incanter/incanter-io "1.9.3-SNAPSHOT"]
+                 [steve-incanter/incanter-charts "1.9.3-SNAPSHOT"]
+                 [steve-incanter/incanter-mongodb "1.9.3-SNAPSHOT"]
+                 [steve-incanter/incanter-pdf "1.9.3-SNAPSHOT"]
+                 [steve-incanter/incanter-svg "1.9.3-SNAPSHOT"]
+                 [steve-incanter/incanter-latex "1.9.3-SNAPSHOT"]
+                 [steve-incanter/incanter-excel "1.9.3-SNAPSHOT"]
+                 [steve-incanter/incanter-sql "1.9.3-SNAPSHOT"]
+                 [steve-incanter/incanter-zoo "1.9.3-SNAPSHOT"]
                  [swingrepl "1.3.0"
                   :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]
                  [org.clojure/clojure "1.8.0"]
